7c9bc1f74a5bdd25f33dbcb5f85c69ff8fbd9515
esther <eslora96@gmail.com>
2018-12-02 00:06:59 +0100

    Done EpsilonGreedy bandit Done hist TODO: Print algorithm progress

--- src/bandits.py
+++ src/bandits.py
@@ -1,103 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-from evaluation import Evaluation
+import seaborn as sns
 
-class Bandit(object):
+class Bandit():
     """
     """
-    k = 1 # Number of items to recommend
-    def __init__(self, exploit_algorithm, splitter):
-        self.exploit_algorithm = exploit_algorithm
-        self.splitter = splitter
-        self.reco_items = []
-        self.is_bandit = []
 
-    def exploitation(self):
-        """
-        """
+    def __init__(self, splitter, criteria="mean"):
+        splitter = splitter
+        self.actions = {item: {"count": 0, "value": 0} for item in splitter.item_set}
+        self.max_item = 0
+        self.max_item_value = 0
+
+        while splitter.test_set:
+            for user in splitter.user_set:
+                if user in splitter.test_set.keys():
+                    while True:
+                        item = self.select_item(splitter)
+                        # Check if we have info about the item and if we haven't reccomended the item
+                        # before to the same user
+                        if (user not in splitter.train_set \
+                            or item not in splitter.train_set[user])\
+                            and item in splitter.test_set[user]:
+
+                            self.actions[item]["count"] += 1
+                            if splitter.test_set[user][item]:
+                                current_value = self.actions[item]["value"]
+                                n = self.actions[item]["count"]
+                                reward = splitter.test_set[user][item]
+                                # Update the item info
+                                if criteria == "mean":
+                                    self.actions[item]["value"] = 1/n*((n-1)*current_value + reward)
+                                elif criteria == "cummulated_mean":
+                                    self.actions[item]["value"] = 1/2*(current_value + reward)
+                                else:
+                                    self.actions[item]["value"] = criteria(n, current_value, reward)
+                            if self.actions[item]["value"] > self.max_item_value:
+                                self.max_item_value = self.actions[item]["value"]
+                                self.max_item = item
+
+                            # Remove item from the test_set and add it to the train_set
+                            reward = splitter.test_set[user].pop(item)
+                            if not splitter.test_set[user]:
+                                splitter.test_set.pop(user)
+                            try:
+                                splitter.train_set[user][item] = reward
+                            except KeyError:
+                                splitter.train_set[user] = {item: reward}
+                            break
+
+    def select_item(self, splitter):
         pass
 
-    def random_sample_filter(self, user, item_set, k):
-        sample = set()
-        i = 0
-        while i<k:
-            item = random.sample(item_set, 1)[0]
-            if ( item not in self.splitter.train[user] and user not in self.splitter.train_r[item] )\
-                and (user != item) and (item not in self.splitter.train_miss[user])\
-                and item not in sample:
-                sample.add(item)
-                i+=1
-        return sample
-
-    def process(self, T):
-        """
-        """
-        results = []
-        for _ in range(T):
-            # Random select user from the train set
-            user = random.sample(self.splitter.train.keys(), 1)
-            if self.exploitation():
-                # If the bandit selects exploitation we use the recommender
-                # algorithm to select k items for the user
-                # self.reco_items.append(list( self.exploit_algorithm.recommend(self.k, user).items()) )
-                self.reco_items.append( self.exploit_algorithm.recommend(self.k, user) )
-                self.is_bandit.append(False)
-            else:
-                # Otherwise we get k random items
-                self.reco_items.append( {u: self.random_sample_filter(u, self.splitter.train_r.keys(), self.k ) for u in user} )
-                self.is_bandit.append(True)
-            # Compute precision and recall
-            ev = Evaluation(self.splitter.test_len_ini, self.k, self.splitter.test, self.reco_items[-1])
-            results.append(ev)
-
-        return results
+    def plot_hist(self):
+        sns.set()
+        N = len(self.actions.keys())
+        X = list(self.actions.keys())
+        Y1 = [self.actions[action]["count"] for action in self.actions.keys()]
+        Y2 = [self.actions[action]["value"] for action in self.actions.keys()]
+        fig, (f1, f2) = plt.subplots(2, 1)
+        f1.bar(range(N), Y1, tick_label = X)
+        f1.set_title("Number of times selected")
+        f2.bar(range(N), Y2, tick_label = X)
+        f2.set_title("Estimated value")
+        plt.show()
 
 class EpsilonGreedyBandit(Bandit):
-    """ Selects the best leveler arm with probability 1-eps
-    """
-    def __init__(self, exploit_algorithm, splitter, eps):
-        """
-        """
-        self.eps = eps
-        Bandit.__init__(self, exploit_algorithm, splitter)
-
-    def exploitation(self):
-        """
-        """
-        return np.random.binomial(1, 1-self.eps)
-
-def plot_results(results):
     """
     """
-    plt.style.use('seaborn')
-    c_prec = [0]
-    c_rec = [0]
-    t = [0]
-    fig, ( prec, rec ) = plt.subplots(2,1)
 
-    for ev in results:
-        c_prec.append(c_prec[-1]+ev.precision)
-        c_rec.append(c_rec[-1]+ev.recall)
-        t.append(t[-1]+1)
+    def __init__(self, epsilon, splitter, criteria="mean"):
+        self.epsilon = epsilon
+        super().__init__(splitter, criteria=criteria)
 
-    prec.plot(t, c_prec)
-    rec.plot(t, c_rec)
-    prec.set_ylabel("Cumulated precision")
-    prec.set_xlabel("t")
+    def select_item(self, splitter):
+        if np.random.binomial(1, 1-self.epsilon):
+            # Eploitation
+            item = self.max_item
+        else:
+            # Exploration
+            item = random.sample(splitter.item_set, 1)[0]
+        return item
 
-    rec.set_ylabel("Cumulated recall")
-    rec.set_xlabel("t")
-    plt.show()
 
-if __name__ == "__main__":
-    from splitter import PercentageSplitter
-    from algorithms import PopularityAlgorithm
+if __name__=="__main__":
+    from splitter import Splitter
 
-    spl = PercentageSplitter("../data/interactions-graph-200tweets.tsv",0.2)
-
-    alg = PopularityAlgorithm(spl)
-    bandit = EpsilonGreedyBandit(alg, spl, 0.2)
-    results = bandit.process(100)
-
-    plot_results(results)
+    spl = Splitter("../data/ratings_binary.txt", " ")
+    bandit = EpsilonGreedyBandit(0.1, spl)
+    bandit.plot_hist()
--- src/splitter.py
+++ src/splitter.py
@@ -1,128 +1,79 @@
-# TODO: Implementar estrategias de bandits
-# TODO: Probar bandits con KNN
-# TODO: Reescribir la implementacion de las clases usando el modelo de FAA
-# TODO: Tener en cuenta el numero de interacciones entre 2 usuarios?
-# TODO: Seleccion de un usuario al azar. IDEA: implementar una funcion que devuelva la lista de los usuarios sobre los que
-#       recomendar para poder hacerlo escalable
-# TODO: OJO con los ficheros de ratings, comprobar que el rating no vale 0
 import numpy as np
 
 class Splitter(object):
     """
 
     """
-
-
-    def addItem(self, user, item, info, destination_set):
-        if destination_set == "train":
-            set1 = self.train
-            set2 = self.test
-            length = self.train_len
-            set1_r = self.train_r
-        else:
-            set1 = self.test
-            set2 = self.train
-            length = self.test_len
-            set1_r = self.test_r
-
-
-        try:
-            # if item not in set1[user]:
-            set1[user][ item ] = info
-            length+=1
-        except KeyError:
-            set1[user] = { item: info }
-            length+=1
-
-        # if item not in set1:
-        #     set1[item] = set()
-
-        # if item not in set2:
-        #     set2[item] = set()
-
-        if user not in set2:
-            set2[user] = {}
-        try:
-            set1_r[item][ user ] = info
-        except:
-            set1_r[item] = { user: info }
-        return length
-
-    def __init__(self, datapath, arg, separator="\t"):
-
-        self.train = {}
-        self.train_miss = {}
+    def __init__(self, path, separator='\t'):
         self.train_len = 0
-        self.rain_len_ini = {}
-        self.train_r = {}
-        self.test = {}
         self.test_len = 0
-        self.test_len_p = 0
-        self.train_len_p = 0
-        self.test_len_ini = 0
-        self.test_r = {}
-        self.data = open(datapath, 'r')
+        self.user_set = set()
+        self.item_set = set()
+        self.train_set = {}
+        self.test_set = {}
 
-        # Removes the data head
-        self.data.readline()
+        with open(path) as data:
+            for line in data:
+                inter = line.split(separator)
+                user = int(inter[0])
+                item = int(inter[1])
+                value = int(inter[2])
+                self.user_set.add(user)
+                self.item_set.add(item)
+                if self.condition():
+                    try:
+                        if item not in self.train_set[user].keys():
+                            self.train_set[user][item] = value
+                    except KeyError:
+                        self.train_set[user] = {item: value}
+                    self.train_len += 1
+                else:
+                    try:
+                        if item not in self.test_set[user].keys():
+                            self.test_set[user][item] = value
+                    except KeyError:
+                        self.test_set[user] = {item: value}
+                    self.test_len += 1
 
-        # Start reading the data file line by line
-        for line in self.data:
 
-            inter = line.split(separator)
-            user = int( inter[0] )
-            item = int( inter[1] )
-            info = int(inter[2])
-            if user != item:
+    def condition(self):
+        """
+        """
+        return False
 
-                if self.condition(arg,info):
-                    self.train_len = self.addItem(user, item, info, "train")
-
-                elif (user not in self.train or item not in self.train[user])\
-                and (item not in self.train or user not in self.train[item])\
-                and (item not in self.test or user not in self.test[item]):
-                    self.test_len = self.addItem(user, item, info, "test")
-
-        self.test_len_ini = self.test_len
-
-        for user in self.train:
-            self.train_miss[user] = set()
-
-        self.data.close()
-
-    def condtion(self, arg1, arg2):
-        return True
-
-class TimestampSplitter(Splitter):
-    """
-    """
-    def condition(self, timestamp, timestamp_r):
-        return timestamp_r <= timestamp
+# class TimestampSplitter(Splitter):
+#     """
+#     """
+#     def condition(self, timestamp, timestamp_r):
+#         return timestamp_r <= timestamp
 
 class RandomSplitter(Splitter):
     """
 
     """
-    def condition(self, p, arg):
-        return np.random.binomial(1, p)
+    def __init__(self, path, prob):
+        self.prob = prob
+        super().__init__(path)
+
+    def condition(self):
+        return np.random.binomial(1, self.prob)
 
 class PercentageSplitter(Splitter):
     """
     """
-    def condition(self, p, arg):
+    def __init__(self, path, percentage):
+        self.percentage = percentage
+        super().__init__(path)
+
+    def condition(self):
         try:
-            return self.train_len/(self.test_len+self.train_len) < p
+            return self.train_len/(self.test_len+self.train_len) < self.percentage
         except ZeroDivisionError:
             return True
 
 if __name__ == "__main__":
-    # spl = TimestampSplitter("../data/interactions-graph-200tweets_100.tsv",1277496627000 )
-    spl = PercentageSplitter("../data/interactions-graph-200tweets.tsv",0.2)
-    # spl = PercentageSplitter("../data/ratings_binary.txt", 0.2, separator=" ")
-    # spl = RandomSplitter("../data/interactions-graph-200tweets_100.tsv", 0.2)
-    print ("TRAIN SET:")
-    # print (spl.train)
-    print (spl.train_len)
-    print ("TEST SET:")
-    # print (spl.test)
-    print (spl.test_len)
+    spl = Splitter("../data/ratings_binary_mini.txt", " ")
+    print(spl.train_set)
+    print(spl.test_set)
+    print(spl.user_set)
+    print(spl.item_set)
