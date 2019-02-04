ae308580b26e449ae514b8d6741689640c4d0c28
esther <eslora96@gmail.com>
2019-02-04 08:03:25 +0100

    Grid search

--- src/main.py
+++ src/main.py
@@ -1,44 +1,41 @@
-#TODO: Cambiar el formateo de print a la version python 3.X
-#TODO: Hacer que cuando la recomendacion no llegue a k, rellene con random
 if __name__ == "__main__":
-    from splitter import TimestampSplitter, RandomSplitter, PercentageSplitter
-    from evaluation import Evaluation
-    from addition import HitAddition
-    from strategy import UniformRandomStrategy, PopularityStrategy
+    from splitter import Splitter
+    from bandits import EpsilonGreedyBandit, UCBBandit, ThompsonSamplingBandit
+    from plot import plot_results_graph
+    import matplotlib.pyplot as plt
+    from numpy import arange
+    from copy import deepcopy
 
-    # i=0
-    # spl = RandomSplitter("../data/interactions-graph-200tweets_100.tsv", 0.1)
-    # spl = TimestampSplitter("../data/interactions-graph-200tweets_100k.tsv", 1357685061000)
-    # spl = TimestampSplitter("../data/interactions-graph-200tweets.tsv", 1357685061000)
-    # spl = TimestampSplitter("../data/interactions-graph-200tweets_100.tsv",1310147215000 )
-    spl = PercentageSplitter("../data/interactions-graph-200tweets_100.tsv", 0.2)
-    k = 10
-    #s = UniformRandomStrategy(spl)
-    s = PopularityStrategy(spl)
-    ev = Evaluation(spl.test_len_ini,k )
-    ad = HitAddition()
-    before = 0
-    pr = open("../results/07-30-18_popularity.txt", "w")
-    pr.write("iteration\tprecision\trecall\thits\tcoverage\n")
-    # while before != spl.train_len:
-    for i in range(100):
-        # print('--------TRAIN SET--------')
-        # print( spl.train )
-        # print( spl.train_len )
-        before = spl.train_len
-        # print('--------TEST SET--------')
-        # print( spl.test_len )
-        # print( spl.test )
-        # print('--------MISSES--------')
-        #  print( spl.train_miss )
-        # print( spl.test_len )
-        # print('--------TOTAL--------')
-        # print( spl.train_len + spl.test_len )
-        # print('--------RECOMMENDATION--------')
-        print(i)
-        reco = s.process(k)
-        # print( len(reco) )
-        ev.evaluate(spl.test, reco)
-        pr.write("%d\t%f\t%f\t%d\t%f\n" % ( i, ev.precision, ev.recall,ev.hits,ev.coverage ))
-        # i+=1
-        ad.add(spl, reco)
+    spl = Splitter("../data/ratings_binary.txt", " ")
+    #for eps in [round(0.1*i, 2) for i in range(11)]:
+    #    bandit = EpsilonGreedyBandit(deepcopy(spl), epsilon = eps, criteria = "cummulative_mean")
+    #    bandit.output_to_file("../results/gridSearch/eps{0}_epoch_cm100_wmean.txt".format(eps),
+    #                          "../results/gridSearch/eps{0}_recall_cm100_wmean.txt".format(eps))
+
+    for alpha in [10**i for i in range(-2, 4)]:
+        bandit = UCBBandit(deepcopy( spl ), param = alpha)
+        bandit.output_to_file("../results/gridSearch/ucb{0}_epoch_cm100_mean.txt".format(alpha),
+                              "../results/gridSearch/ucb{0}_recall_cm100_mean.txt".format(alpha))
+    #fig = plt.figure()
+    #for eps in [round(0.1*i, 2) for i in range(11)]:
+    #        res_file = "../results/gridSearch/eps{0}_recall_cm100_wmean.txt".format(eps)
+    #        plot_results_graph(res_file, "eps={}".format(eps))
+
+    #plt.legend()
+    #plt.savefig("../results/Recall.png")
+    #plt.show()
+    #spl = Splitter("../data/ratings_binary.txt", " ")
+    #bandit = UCBBandit(spl, criteria="cummulative_mean")
+    #bandit.output_to_file("../results/ucb2_epoch_cm100_wmean.txt",
+    #                      "../results/ucb2_recall_cm100_wmean.txt")
+
+    #spl = Splitter("../data/ratings_binary.txt", " ")
+    #eps = 0.1
+    #bandit = EpsilonGreedyBandit(spl, criteria="cummulative_mean")
+    #bandit.output_to_file("../results/eps{0}_epoch_cm100_wmean.txt".format(eps),
+    #                      "../results/eps{0}_recall_cm100_wmean.txt".format(eps))
+
+    #spl = Splitter("../data/ratings_binary.txt", " ")
+    #bandit = ThompsonSamplingBandit(spl, criteria="cummulative_mean")
+    #bandit.output_to_file("../results/ts_epoch_cm100_wmean.txt",
+    #                      "../results/ts_recall_cm100_wmean.txt")
