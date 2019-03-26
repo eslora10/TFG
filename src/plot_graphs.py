3d31f5aa0f82c52b3457ad089fccee5737627b7e
esther <eslora96@gmail.com>
2019-03-26 15:58:08 +0100

    plot_graphs.py modified to plot the contour

--- src/plot_graphs.py
+++ src/plot_graphs.py
@@ -1,9 +1,13 @@
+import numpy as np
+import matplotlib.colors as cl
+import scipy.ndimage
 from plot import plot_results_graph
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 path = "../results/gridSearch/ucb/cm100k/param/ucb{0}_recall_cm100_miniwmean.txt"
-alg = "UCB"
+#path = "../results/gridSearch/eps/cm100k/param/eps{0}_recall_cm100_wmean.txt"
+alg = "Epsilon"
 
 #values = [0.0, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
 values = [0, 0.1, 1, 2, 10, 100]
@@ -16,7 +20,7 @@ colors = sns.hls_palette(len(values)+1, l=.4, s=.8)
 ax.set_prop_cycle('color', colors)
 for value in values:
     filename = path.format(value)
-    plot_results_graph(filename, r"$\gamma$={0}".format(value))
+    plot_results_graph(filename, r"$\varepsilon$={0}".format(value))
 
 plot_results_graph("../results/gridSearch/random_cm100k.txt", "random")
 plt.legend()
@@ -32,7 +36,9 @@ colors = sns.hls_palette(len(values)+1, l=.4, s=.8)
 ax.set_prop_cycle('color', colors)
 X = []
 Y = []
-with open("../results/gridSearch/ucb/cm100k/param/ucb_cut.txt") as input_file:
+path = "../results/gridSearch/ucb/cm100k/param/ucb_cut.txt"
+#path = "../results/gridSearch/eps/cm100k/param/eps_param.txt"
+with open(path) as input_file:
     for line in input_file:
         spl = line.strip("\n").split(",")
         x = float(spl[0])
@@ -44,4 +50,37 @@ plt.plot(X, Y, color="black", linewidth = .8, zorder = -1)
 plt.xlabel(r"$\gamma$")
 plt.ylabel("Recall mitad")
 plt.savefig("../results/memoria/cm100k/RecallMitad"+ alg +".png")
+plt.show()
+path = "../results/gridSearch/ucb/cm100k/optimistic/ucb_cut.txt"
+#path = "../results/gridSearch/eps/cm100k/optimistic/eps_cut.txt"
+with open(path, "r") as entrada:
+    Z = []
+    i = 0
+    sub = []
+    zvals = []
+    for line in entrada:
+        vals = line.strip("\n").split(",")
+        zvals.append(float(vals[2]))
+        if i == 10:
+            Z.append(sub)
+            sub = []
+            i = 0
+        sub.append(float(vals[2]))
+        i+=1
+Z.append(sub)
+sns.set()
+sns.set_context("paper")
+sns.set_style("white")
+vmin = min(zvals)
+vmax = max(zvals)
+Z = scipy.ndimage.zoom(Z, 3)
+X = np.arange(1,11, 10/30)
+plt.contour(X,X,Z, colors=["black"], linewidths=[0.8])
+plt.contourf(X,X,Z, cmap=plt.get_cmap("RdBu"), norm=cl.Normalize(vmin=vmin, vmax=vmax))
+plt.colorbar(fraction=0.05)
+plt.ylabel(r"$\alpha$")
+plt.xlabel(r"$\beta$")
+plt.xlim((1,10))
+plt.ylim((1,10))
+plt.savefig("../results/memoria/cm100k/Mapa"+ alg +".png")
 plt.show()
